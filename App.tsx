5e77538769401477fed2cbaf683152a4deefea2d
John <ST10530092@myemeris.edu.za>
2026-08-11 13:35:56 +0200

    implicated navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,30 @@
 import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator ();
+
 export default function App() {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={PlayerScreen}/>
+        <Stack.Screen name="Game" component={GameScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+  function PlayerScreen({ navigation }: any) {
   const [player1, setPlayer1] = useState<string>('');
   const [player2, setPlayer2] = useState<string>('');
-
 
   return (
     <View style={styles.container}>
      <text style={styles.title}>Welcome to Tic-Tac-Toe!</text>
+
       <TextInput 
         placeholder='Player 1 Name'
         value={player1}
@@ -23,12 +39,18 @@ export default function App() {
       />
       <Button 
       title="Start Game"
-      onPress={() => console.log(`Player1: ${player1}, Player 2: ${player2}`)}
-      
+      onPress={() => navigation.navigate('Game')}
       />
-
     </View>
   );
+}
+
+function GameScreen({ navigation,route}: any) {
+  return(
+    <View style={styles.container}>
+      <Text> Player 1 vs Player 2</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
